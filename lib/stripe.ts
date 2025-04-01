44d6c817b8885d14370fa9bb8a0706e11501a4a2
João Filipe Romão <jrmromao@gmail.com>
2025-04-01 17:12:36 +0100

    Remove stripe secret key

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,7 +2,7 @@
 import Stripe from 'stripe';
 
 // Make sure this is your SECRET key, not your publishable key
-const secretKey = "sk_test_51PifCH2N5SBY44N5QTQZWyJn8oxmCVaYkWDUWXGmr5Yp2fmlwWo4SUKtpai2tC2ku8TkJ9Y3FBrLyeMQM7ufS8pE00rJwIf2bW";
+const secretKey = process.env.STRIPE_SECRET_KEY;
 
 if (!secretKey) {
     throw new Error('Missing STRIPE_SECRET_KEY environment variable');
