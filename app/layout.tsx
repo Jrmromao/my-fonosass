53f091e1953667ab55b7a133964f7224b91b0204
João Filipe Romão <jrmromao@gmail.com>
2025-05-31 15:05:44 +0100

    Add Vercel analystics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import {ClerkProvider} from "@clerk/nextjs";
 import { ptBR } from '@clerk/localizations'
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import {Providers} from "@/components/Providers";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({subsets: ["latin"]})
 
@@ -24,6 +25,7 @@ export default function RootLayout({
                 className={`${inter.className} flex flex-col min-h-screen bg-gradient-to-br from-indigo-100 via-white to-purple-100`}>
             <Providers>
                 {children}
+                <Analytics />
             </Providers>
             <SpeedInsights />
             </body>
